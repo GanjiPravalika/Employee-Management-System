1598a36a801d5e6225c72e640a616cac49efd6b5
GanjiPravalika <ganjipravalika57@gmail.com>
2026-07-17 10:42:07 +0530

    Delete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Setting up the Database for the application...."
-echo "Make sure you have the mysql server running and mysql client installed on your machine"
-
-mysql -u root -p < /Resources/Script.sql
